afa24605a5934f962844711e429298083e2e2149
vagrant <vagrant@vagrant.vm>
2020-10-01 01:37:50 +0000

    Creates print produce summary function

--- produce_summary.py
+++ produce_summary.py
@@ -1,17 +1,20 @@
+def print_produce_summary(the_file):
+    the_file = open(the_file)
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount))
+
+    the_file.close()
+
 print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
+print_produce_summary("um-deliveries-20140519.txt")
 
 print("Day 2")
 the_file = open("um-deliveries-20140520.txt")
